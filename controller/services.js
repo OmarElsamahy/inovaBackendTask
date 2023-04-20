efedee4684a5bc2d8b6c6e5409389f2d812e5d58
unknown <omarelsamahy109@gmail.com>
2023-04-20 15:12:38 +0200

    controller

--- controller/services.js
+++ controller/services.js
@@ -1,10 +1,11 @@
 const { Post, Review } = require("../model/db");
-const sequelize = require("sequelize");
+
+const { sequelize } = require("../config/dbconfig");
 module.exports = {
   async createPost(req, res) {
     try {
-      const { title, body, userId } = req.body;
-      const post = await Post.create({ title, body, userId });
+      const { title, body, UserId } = req.body;
+      const post = await Post.create({ title, body, UserId });
       res.status(201).json(post);
     } catch (err) {
       console.error(err);
@@ -14,12 +15,12 @@ module.exports = {
 
   async listUserPosts(req, res) {
     try {
-      const { userId } = req.params;
+      const { UserId } = req.params;
       const { page, pageSize } = req.query;
       const offset = (page - 1) * pageSize;
       const limit = pageSize;
       const posts = await Post.findAndCountAll({
-        where: { userId },
+        where: { UserId },
         offset,
         limit,
       });
@@ -42,7 +43,7 @@ module.exports = {
           AVG("Reviews".rating) AS "averageRating", 
           COUNT("Reviews".id) AS "reviewCount"
         FROM "Posts" AS "Post"
-        LEFT JOIN "Reviews" AS "Reviews" ON "Post".id = "Reviews"."postId"
+        LEFT JOIN "Reviews" AS "Reviews" ON "Post".id = "Reviews"."PostId"
         GROUP BY "Post".id
         ORDER BY "averageRating" DESC
         LIMIT :limit OFFSET :offset
@@ -60,18 +61,18 @@ module.exports = {
   },
 
   async addPostReview(req, res) {
-    const postId = req.params.id;
-    const { rating, comment, userId } = req.body;
+    const PostId = req.params.id;
+    const { rating, comment, UserId } = req.body;
     const t = await sequelize.transaction();
     try {
       // Check if the post exists
-      const post = await Post.findByPk(postId);
+      const post = await Post.findByPk(PostId);
       if (!post) {
         return res.status(404).json({ error: "Post not found" });
       }
       // Add the review to the post
       const review = await Review.create(
-        { rating, comment, postId, userId },
+        { rating, comment, PostId, UserId },
         { transaction: t }
       );
       await t.commit();
